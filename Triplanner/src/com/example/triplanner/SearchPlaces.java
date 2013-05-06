53b9a32291c3efb5b6344f20007696ecb70fce5e
claire921 <claire921@gmail.com>
2013-05-06 15:41:40 -0400

    add tripplanner

---
+++ Triplanner/src/com/example/triplanner/SearchPlaces.java
@@ -0,0 +1,15 @@
+package com.example.triplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchPlaces extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		  super.onCreate(savedInstanceState);
+	        
+	      setContentView(R.layout.searchplaces); 
+		
+	} 
+}
---
+++ Triplanner/src/com/example/triplanner/ShowRoute.java
@@ -0,0 +1,13 @@
+package com.example.triplanner;
+
+import android.app.Activity;
+import android.os.Bundle; 
+
+public class ShowRoute extends Activity{  
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		  super.onCreate(savedInstanceState);
+	        
+	      setContentView(R.layout.showroute); 
+	}
+}
